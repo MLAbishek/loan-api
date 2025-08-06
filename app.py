1b47746e7efe9e04d8eace06078bbb3382dec9e8
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-08-06 11:34:03 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from preprocessor import preprocess_data
 from train import train_model
 import pandas as pd
 import traceback
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -83,4 +85,5 @@ def retrain():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
